9b2874d400649b571e0a71a268c8d15304fc14a9
amirsmagul <amir.smagul01@gmail.com>
2024-07-02 14:54:46 +0200

    the HomeFragment design is updated and added RecyclerView adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,13 +43,19 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+
     //firebase
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore:25.0.0")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("com.android.car.ui:car-ui-lib:2.6.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    //material design
+    implementation("com.google.android.material:material:1.7.0")
 
 }
--- app/src/main/java/com/example/projectwork/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/projectwork/Fragments/HomeFragment.java
@@ -1,28 +1,44 @@
 package com.example.projectwork.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.projectwork.Adapters.homeFragmentRecyclerAdapter;
+import com.example.projectwork.DataClasses.WasteDisposalItem;
 import com.example.projectwork.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<WasteDisposalItem> items = new ArrayList<>();
+        items.add(new WasteDisposalItem(R.drawable.img_1, "Paper"));
+        items.add(new WasteDisposalItem(R.drawable.img_2, "Metal"));
+        items.add(new WasteDisposalItem(R.drawable.img_3, "Plastic"));
+        items.add(new WasteDisposalItem(R.drawable.img_4, "Glass"));
+
+
+        homeFragmentRecyclerAdapter adapter = new homeFragmentRecyclerAdapter(items);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
