e3f139f2425105f72ae0e631b7f47a710f499381
sunra1z <tokaev.04@gmail.com>
2024-07-17 22:20:47 +0200

    Tasks beta (xml and etc)

--- app/src/main/java/com/example/projectwork/Fragments/TasksFragment.java
+++ app/src/main/java/com/example/projectwork/Fragments/TasksFragment.java
@@ -1,6 +1,7 @@
 package com.example.projectwork.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.projectwork.R;
+import com.example.projectwork.TaskUploadActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TasksFragment extends Fragment {
+
+    FloatingActionButton createTask;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        createTask = view.findViewById(R.id.upload_task_button);
+
+        createTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TaskUploadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/projectwork/TaskUploadActivity.java
@@ -0,0 +1,18 @@
+package com.example.projectwork;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TaskUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_upload);
+        }
+    }
